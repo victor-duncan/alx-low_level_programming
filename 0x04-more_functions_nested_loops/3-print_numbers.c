587094931d8dda613ee9b5d4b52d5a805aef9c25
Victor-duncan <ahortuvictorduncan@gmail.com>
2022-04-15 19:57:53 -0700

    alx low level programming

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,12 +4,14 @@
  * print numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
+
 void print_numbers(void)
 {
 	int i;
-	for (i = 0; i < 10; i++)
+
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 
 	_putchar('\n');
