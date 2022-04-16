6552d99e964795bbcde52e7b6f8577d18d2f483e
Victor-duncan <ahortuvictorduncan@gmail.com>
2022-04-15 19:41:23 -0700

    alx low level programming

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * mul - function that multiplies two integers.
- *
+ * mul - multiplies two integers
  * @a: int to be multiplied to b
  * @b: int to be multiplied to a
- *
  * Return: the result of the operation
  */
-
 int mul(int a, int b)
 {
 	int c;
 
 	c = a * b;
 
-	return (c)
-
+	return (c);
+}
