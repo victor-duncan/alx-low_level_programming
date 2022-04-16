cb538e3cfb0a3a3fbfcf39b3d1428632854a5ac3
Victor-duncan <ahortuvictorduncan@gmail.com>
2022-04-15 20:08:53 -0700

    alx low level programming

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_most_numbers - function that prints the numbers, from 0 to 9.
- * Do not print 2 and 4
- *
- * Return: Always 0 (Success).
+ * print_most_numbers - a function that prints the numbers, from 0 to 9.
+ * Do not print 2 and 4.
+ * followed by a new line.
+ * Return: Always 0
  */
-
 void print_most_numbers(void)
 {
-	int i;
+	int i = '0';
 
-	for (i = 0; i <= 9; i++)
+	for (; i <= '9'; i++)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar('0' + i);
-		}
+		for (; i <= '9'; i++)
+			_putchar(i);
 	}
 	_putchar('\n');
 }
