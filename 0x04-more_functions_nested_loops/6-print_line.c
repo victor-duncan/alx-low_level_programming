09fc95ee39f42492d699183fe678383788c6f47a
Victor-duncan <ahortuvictorduncan@gmail.com>
2022-04-15 22:06:16 -0700

    alx low level programming

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * print_line -function that draws a straight line in the terminal.
- *
- * @n: n is the number of time the character _ should be printed.
- *
- * Return: Always 0 (Success).
+ * print_line - a function that draws a straight line in the terminal.
+ * followed by a new line.
+ * @n: An input integer
+ * Return: Always 0
  */
-
 void print_line(int n)
 {
 	int i = 0;
-	
+
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (; i < n; i++)
 			_putchar('_');
 	}
 	_putchar('\n');
